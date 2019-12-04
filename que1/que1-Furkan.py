fb0bcdd0437cae745cd25edcac038b57f57dfcb1
Oğuzhan <54781138+oguzhnatly@users.noreply.github.com>
2019-12-04 18:09:59 +0300

    Update que1-Furkan.py
    That's better

--- que1/que1-Furkan.py
+++ que1/que1-Furkan.py
@@ -1,16 +1,22 @@
-def KaplumbagaYarisi(speed_a,speed_b,step):
+speed_a = int(float(input("Please enter the speed of the Turtle A: ")))
+speed_b = int(float(input("Please enter the speed of the Turtle B: ")))
+step = int(float(input("Please enter the number of steps: ")))
+
+def TurtleRacing():
+    global speed_a, speed_b, step
     if speed_a > speed_b:
-        reach_time = (step)/(speed_a - speed_b)
-        sec_reach_time = reach_time*3600
-        second = sec_reach_time % 60
-        minute = sec_reach_time // 60
+        time = (step / (speed_b - spB)) * 3600
+        second = time % 60
+        minute = time // 60
         hour = 0
         if minute >= 60:
             hour = minute // 60
             minute = minute % 60
-
         return [hour,minute,second]
     else:
         return [-1,-1,-1]
 
-print(KaplumbagaYarisi(23,20,80))
+if spA == "" and spB == "" and step == "":
+    print("Please enter an int value!")
+else:
+    print(TurtleRacing())
